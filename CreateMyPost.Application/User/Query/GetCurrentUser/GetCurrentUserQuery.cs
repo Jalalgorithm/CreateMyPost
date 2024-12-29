15ba1e7c82f7bf8329571ccd4dde842acc17ab85
Jalalgorithm <temitomzi@gmail.com>
2024-12-29 18:05:04 +0100

    Implemented CurrentUser Handler

---
+++ CreateMyPost.Application/User/Query/GetCurrentUser/GetCurrentUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using CreateMyPost.Application.Helpers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Application.User.Query.GetCurrentUser
+{
+    public class GetCurrentUserQuery :IRequest<UserResponse>
+    {
+    }
+}
---
+++ CreateMyPost.Application/User/Query/GetCurrentUser/GetCurrentUserQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using CreateMyPost.Application.Helpers;
+using CreateMyPost.Domain.Entities;
+using CreateMyPost.Domain.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Application.User.Query.GetCurrentUser
+{
+    public class GetCurrentUserQueryHandler : IRequestHandler<GetCurrentUserQuery, UserResponse>
+    {
+        private readonly ICurrentService _currentUserService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public GetCurrentUserQueryHandler(UserManager<ApplicationUser> userManager, ICurrentService currentUserService)
+        {
+            _userManager = userManager;
+            _currentUserService = currentUserService;
+        }
+        public async Task<UserResponse> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(_currentUserService.UserId());
+
+            if(user is null)
+            {
+                throw new InvalidOperationException("User not found");
+            }
+
+            var response = new UserResponse
+            {
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber ?? "",
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+
+            };
+
+            return response;
+        }
+    }
+}
