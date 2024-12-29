47e1308cef5cdb6647e4e4b1e4be0ee6c156e691
Jalalgorithm <temitomzi@gmail.com>
2024-12-29 17:26:47 +0100

    Implemented GetUserById Command

---
+++ CreateMyPost.Application/User/Query/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using CreateMyPost.Application.Helpers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Application.User.Query.GetUserById
+{
+    public class GetUserByIdQuery : IRequest<UserResponse>
+    {
+        public Guid Id { get; set; }
+
+        public GetUserByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+
+}
---
+++ CreateMyPost.Application/User/Query/GetUserById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using CreateMyPost.Application.Helpers;
+using CreateMyPost.Domain.Entities;
+using CreateMyPost.Domain.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Application.User.Query.GetUserById
+{
+    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserResponse>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ICurrentService _currentService;
+        
+        public GetUserByIdQueryHandler(ICurrentService currentService, UserManager<ApplicationUser> userManager)
+        {
+            _currentService = currentService;
+            _userManager = userManager;
+        }
+        public async Task<UserResponse> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(request.Id.ToString());
+
+            if(user is null)
+            {
+                throw new InvalidOperationException("User not found");
+            }
+
+            var userResponse = new UserResponse
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                PhoneNumber = user.PhoneNumber ?? "",
+            };
+
+            return userResponse;
+
+        }
+    }
+}
