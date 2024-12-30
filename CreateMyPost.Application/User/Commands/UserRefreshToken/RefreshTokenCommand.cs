2819ed7f5e0acf3fcbc7c16e09277ef4f3150d15
Jalalgorithm <temitomzi@gmail.com>
2024-12-30 15:10:15 +0100

    implemented Refreshtoken handler

---
+++ CreateMyPost.Application/User/Commands/UserRefreshToken/RefreshTokenCommand.cs
@@ -0,0 +1,16 @@
+﻿using CreateMyPost.Application.Helpers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Application.User.Commands.UserRefreshToken
+{
+    public class RefreshTokenCommand : IRequest<UserResponse>
+    {
+        public string RefreshToken { get; set; }
+
+    }
+}
---
+++ CreateMyPost.Application/User/Commands/UserRefreshToken/RefreshTokenCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using CreateMyPost.Application.Helpers;
+using CreateMyPost.Domain.Entities;
+using CreateMyPost.Domain.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Application.User.Commands.UserRefreshToken
+{
+    public class RefreshTokenCommandHandler : IRequestHandler<RefreshTokenCommand, UserResponse>
+    {
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RefreshTokenCommandHandler(ITokenService tokenService, UserManager<ApplicationUser> userManager)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+        }
+        public async Task<UserResponse> Handle(RefreshTokenCommand request, CancellationToken cancellationToken)
+        {
+            using var sha256 = SHA256.Create();
+            var refreshToken = sha256.ComputeHash(Encoding.UTF8.GetBytes(request.RefreshToken));
+            var hashedRefreshToken = Convert.ToBase64String(refreshToken);
+
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.RefreshToken == hashedRefreshToken);
+
+            if(user is null)
+            {
+                throw new InvalidOperationException("User is not found");
+            }
+
+            if(user.RefreshTokenExpires < DateTime.Now)
+            {
+                throw new InvalidOperationException("Refresh token expired");
+            }
+
+            var newAccessToken = await _tokenService.GenerateToken(user);
+
+            var response = new UserResponse
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = newAccessToken,
+
+            };
+
+            return response;
+        }
+    }
+}
