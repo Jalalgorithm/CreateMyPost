4e335732fd19c23329a2433ebb4e7d1cc8d17496
Jalalgorithm <temitomzi@gmail.com>
2024-12-31 10:33:00 +0100

    Implemented Delete User Handler

---
+++ CreateMyPost.Application/User/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using CreateMyPost.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Application.User.Commands.DeleteUser
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public DeleteUserCommandHandler(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(request.Id.ToString());
+
+            if(user is null)
+            {
+                throw new InvalidOperationException("User not found");
+            }
+
+            await _userManager.DeleteAsync(user);
+        }
+    }
+}
