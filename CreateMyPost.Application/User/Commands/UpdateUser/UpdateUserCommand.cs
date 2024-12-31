dda84262bced919a6af3578b7224fd5e6cd96119
Jalalgorithm <temitomzi@gmail.com>
2024-12-31 10:27:23 +0100

    Implemented Update User Handler

---
+++ CreateMyPost.Application/User/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,24 @@
+﻿using CreateMyPost.Application.Helpers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Application.User.Commands.UpdateUser
+{
+    public class UpdateUserCommand : IRequest<UserResponse>
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; }=string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public UpdateUserCommand(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ CreateMyPost.Application/User/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using CreateMyPost.Application.Helpers;
+using CreateMyPost.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Application.User.Commands.UpdateUser
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserResponse>
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        public UpdateUserCommandHandler(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public async Task<UserResponse> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await userManager.FindByIdAsync(request.Id.ToString());
+
+            if(user is null)
+            {
+                throw new InvalidOperationException("User not found");
+            }
+
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.PhoneNumber = request.PhoneNumber;
+            
+           await userManager.UpdateAsync(user);
+
+            var response = new UserResponse
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.LastName
+            };
+
+            return response;
+        }
+    }
+}
