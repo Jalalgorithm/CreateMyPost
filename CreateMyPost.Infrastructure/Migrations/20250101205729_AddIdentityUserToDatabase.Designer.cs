0532ffd1db28507e734c7c012c78644a8c71b9f5
Jalalgorithm <temitomzi@gmail.com>
2025-01-01 22:27:24 +0100

    Implemented Controller

--- CreateMyPost.Infrastructure/Migrations/20250101205729_AddIdentityUserToDatabase.Designer.cs
+++ CreateMyPost.Infrastructure/Migrations/20250101205729_AddIdentityUserToDatabase.Designer.cs
@@ -4,6 +4,7 @@ using CreateMyPost.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CreateMyPost.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250101205729_AddIdentityUserToDatabase")]
+    partial class AddIdentityUserToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CreateMyPost.WebAPI.Server/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using CreateMyPost.Application.User.Commands.AddUser;
+using CreateMyPost.Application.User.Commands.LogInUser;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreateMyPost.WebAPI.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ISender _sender;
+        public AccountController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody]RegisterUserCommand userCommand)
+        {
+            var response = await _sender.Send(userCommand);
+            return Ok(response);    
+        }
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody]LogInUserCommand userLogin)
+        {
+            var response = await _sender.Send(userLogin);
+            return Ok(response);
+        }
+
+        
+
+        
+    }
+}
