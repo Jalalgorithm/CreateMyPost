6cfeb886c9be593f19822fe707c4826247cd4fd1
Jalalgorithm <temitomzi@gmail.com>
2025-01-04 13:36:08 +0100

    Implemented an email service using smtp

---
+++ CreateMyPost.Domain/Repository/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Domain.Repository
+{
+    public interface IEmailService
+    {
+        Task SendEmail(string recipientName, string recipientEmail, string subject, string body);
+    }
+}
---
+++ CreateMyPost.Infrastructure/Repository/EmailService.cs
@@ -0,0 +1,55 @@
+﻿using CreateMyPost.Domain.Repository;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MailKit.Net.Smtp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Infrastructure.Repository
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendEmail(string recipientName, string recipientEmail, string subject, string body)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+            var senderEmail = emailSettings["SenderEmail"];
+            var senderName = emailSettings["SenderName"];
+            var senderPassword = emailSettings["SenderPassword"];
+            var smtpServer = emailSettings["SmtpServer"];
+            var smtpPort = int.Parse(emailSettings["SmtpPort"]);
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(senderName, senderEmail));
+            message.To.Add(new MailboxAddress(recipientName, recipientEmail));
+            message.Subject = subject;
+            message.Body = new TextPart("plain") { Text = body };
+
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(smtpServer, smtpPort, SecureSocketOptions.StartTls);
+                    await client.AuthenticateAsync(senderEmail, senderPassword);
+                    await client.SendAsync(message);
+                    Console.WriteLine("Email sent successfully!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                }
+            }
+    }
+}
