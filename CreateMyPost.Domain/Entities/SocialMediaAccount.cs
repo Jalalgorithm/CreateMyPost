717c0c116105e2ea2f586d82783837796d81b605
jalalgoirthm <temitomzi@gmail.com>
2025-01-11 21:26:12 +0100

    Added a social media account model

---
+++ CreateMyPost.Domain/Entities/SocialMediaAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMyPost.Domain.Entities
+{
+    public class SocialMediaAccount
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId{ get; set; }
+        public string Platform { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+        public DateTime TokenExpiry { get; set; }
+    }
+}
